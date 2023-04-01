392b51cd65a0887f1ac5f18416f1574806d6d59a
Mathis Zerari <mathiszerari@icloud.com>
2023-04-01 15:11:11 +0200

    for loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { SOCIALMEDIAS } from './mock-social';
 import { Social } from './social';
 
@@ -15,5 +15,16 @@ export class AppComponent implements OnInit {
 
   selectSocial(social: Social) {
     window.open(social.link, '_blank');
+  }
+
+  // ici je crée la condition si la flèche de droite est pressée
+  arrowright: boolean = false;
+  @HostListener('document:keydown.arrowright', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    this.arrowright = true;
+  }
+  @HostListener('document:keyup.arrowright', ['$event'])
+  onKeyUp(event: KeyboardEvent) {
+    this.arrowright = false;
   }
 }
