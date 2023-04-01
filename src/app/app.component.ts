ecc2f674f516d55b68ee9847f8b400d85014c759
Mathis Zerari <mathiszerari@icloud.com>
2023-04-01 22:46:57 +0200

    arrow pressed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,43 @@ export class AppComponent implements OnInit {
 
   // ici je crée la condition si la flèche de droite est pressée
   arrowright: boolean = false;
+  arrowdown: boolean = false;
+  arrowleft: boolean = false;
+  arrowup: boolean = false;
+
   @HostListener('document:keydown.arrowright', ['$event'])
-  onKeyDown(event: KeyboardEvent) {
+  onKeyDownRight(event: KeyboardEvent) {
     this.arrowright = true;
   }
   @HostListener('document:keyup.arrowright', ['$event'])
-  onKeyUp(event: KeyboardEvent) {
+  onKeyUpRight(event: KeyboardEvent) {
     this.arrowright = false;
+  }
+
+  @HostListener('document:keydown.arrowleft', ['$event'])
+  onKeyDownLeft(event: KeyboardEvent) {
+    this.arrowleft = true;
+  }
+  @HostListener('document:keyup.arrowleft', ['$event'])
+  onKeyUpLeft(event: KeyboardEvent) {
+    this.arrowleft = false;
+  }
+
+  @HostListener('document:keydown.arrowup', ['$event'])
+  onKeyDownup(event: KeyboardEvent) {
+    this.arrowup = true;
+  }
+  @HostListener('document:keyup.arrowup', ['$event'])
+  onKeyUpup(event: KeyboardEvent) {
+    this.arrowup = false;
+  }
+
+  @HostListener('document:keydown.arrowdown', ['$event'])
+  onKeyDowndown(event: KeyboardEvent) {
+    this.arrowdown = true;
+  }
+  @HostListener('document:keyup.arrowdown', ['$event'])
+  onKeyUpdown(event: KeyboardEvent) {
+    this.arrowdown = false;
   }
 }
